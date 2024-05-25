40255e7b87983324bcf2d3ae0d98b912daf5bc8d
Dima Dzundza <dzundzan@gmail.com>
2024-05-25 19:47:52 +0300

    update summary text

--- internal/core/balance/utils/summary.go
+++ internal/core/balance/utils/summary.go
@@ -8,6 +8,7 @@ func GenerateDaySummaryString(balance string, statement string) string {
 	sb := strings.Builder{}
 
 	sb.WriteString(balance)
+	sb.WriteString("\n")
 
 	if statement == "" {
 		sb.WriteString("Нет операций за этот день")
